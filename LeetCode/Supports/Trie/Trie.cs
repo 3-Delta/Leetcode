4612080cb27463c90deaeff7189c702dd6eaaadb
kaclok <572005238@qq.com>
2019-09-14 13:01:06 +0800

    trie树

--- LeetCode/Supports/Trie/Trie.cs
+++ LeetCode/Supports/Trie/Trie.cs
@@ -6,4 +6,32 @@ using System.Threading.Tasks;
 
 public class Trie
 {
+    private bool isTrie;
+    private int len;
+    // 所有子节点，供26个
+    public Trie[] tries = new Trie[26];
+
+    public Trie()
+    {
+        isTrie = false;
+    }
+    public void Insert(string str)
+    {
+        if (str.Length == 0)
+        {
+            return;
+        }
+        int i = 0;
+        while (i < str.Length)
+        {
+            if (tries[str[i] - 'a'] == null)
+            {
+                tries[str[i] - 'a'] = new Trie();
+            }
+            ++i;
+        }
+
+        isTrie = true;
+        len = i;
+    }
 }
