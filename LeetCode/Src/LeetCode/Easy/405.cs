24689be35b5240b26a577c413b8db83bdbc9128a
kaclok <572005238@qq.com>
2019-09-19 21:04:25 +0800

    提交

---
+++ LeetCode/Src/LeetCode/Easy/405.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public static class LC_405
+{
+    public static void Func()
+    {
+        char[] chars = new char[]{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+        Console.WriteLine(ToHex(26, chars));
+    }
+    public static string ToHex(int num, char[] chars)
+    {
+        //string ret = "";
+        //while (num != 0)
+        //{
+        //    ret += chars[num % 16];
+        //    num = num / 16;
+        //}
+        //return ret.Reverse();
+    }
+}
