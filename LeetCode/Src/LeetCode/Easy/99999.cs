5ef8315b6e97ce85b01ee4ed6ff4908dc444f408
kaclok <572005238@qq.com>
2019-10-21 21:46:57 +0800

    二分法妙用

---
+++ LeetCode/Src/LeetCode/Easy/99999.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+
+/*
+    https://mp.weixin.qq.com/s/LccoG6IB3ZewNv9qzv_6KQ
+    s = "abc", t = "ahbgdc", return true.
+    s = "axc", t = "ahbgdc", return false.
+ */
+public static class LC_99999
+{
+    public static void Func()
+    {
+        
+    }
+    public static bool IsSubSequence(string[] ss, string t)
+    {
+        foreach (string s in ss)
+        {
+            if (!IsSubSequence(s, t))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static bool IsSubSequence(string s, string t)
+    {
+        int i = 0, j = 0;
+        while (i < s.Length && j < t.Length)
+        {
+            if (s[i] == t[j])
+            {
+                i++;
+            }
+            j++;
+        }
+        return i == s.Length;
+    }
+}
