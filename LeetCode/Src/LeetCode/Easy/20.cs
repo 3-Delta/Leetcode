98c9eb437f44adc632f71856fdc3125eae9a960e
kaclok <572005238@qq.com>
2020-02-22 11:56:20 +0800

    提交

--- LeetCode/Src/LeetCode/Easy/20.cs
+++ LeetCode/Src/LeetCode/Easy/20.cs
@@ -14,6 +14,7 @@ public static class LC_20
     // 最终结果是：判断stack中元素是否为0
     // 如果匹配，stack会出栈
     // 不匹配则继续入栈
+    // On做法
     public static bool Match(string s)
     {
         Stack<char> stack = new Stack<char>();
@@ -37,6 +38,35 @@ public static class LC_20
             }
         }
         return stack.Count <= 0;
+    }
+    // https://mp.weixin.qq.com/s/rG0-2wtWigRJdLb1BwipCg
+    public static bool MatchO1(string s)
+    {
+        bool isMatch = false;
+        if (s != null)
+        {
+            int count = 0;
+            foreach (var c in s)
+            {
+
+                isMatch = true;
+                if (c == '(' || c == '[' || c == '{')
+                {
+                    ++count;
+                }
+                else if (c == ')' || c == ']' || c == '}')
+                {
+                    --count;
+                }
+
+                if (count < 0)
+                {
+                    isMatch = false;
+                    break;
+                }
+            }
+        }
+        return isMatch;
     }
 
     private static bool IsMatch(char left, char right)
