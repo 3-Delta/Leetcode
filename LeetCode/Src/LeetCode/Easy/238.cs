362442e62c415373633a2682853c24d0fb6f60c3
kaclok <kaclok@users.noreply.github.com>
2021-04-21 01:08:20 +0800

    添加LeetCode题目

---
+++ LeetCode/Src/LeetCode/Easy/238.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public static class LC_238 {
+    public static void Func() { }
+
+    public static int[] ProductExceptSelf(int[] nums) {
+        int length = nums.Length;
+        int[] answer = new int[nums.Length];
+
+        // answer[i] 表示0 ~ i-1  所有元素乘积
+        // 注意不是：answer[i] 表示0 ~ i 所有元素乘积，不包括answer[i];
+        answer[0] = 1;
+        for (int i = 1; i < length; ++i) {
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        // r 表示 i+1 到length的所有元素乘积
+        // 倒着相乘
+        int r = 1;
+        for (int i = length - 1; i >= 0; --i) {
+            answer[i] = answer[i] * r;
+            r *= nums[i];
+        }
+
+        return answer;
+    }
+    
+    // 除了nums[i]之外，左右最大值的和
+    // 可以利用两个answer，一个从左到右，一个从右到左配合得到max
+}
---
+++ LeetCode/Src/LeetCode/Middle/287.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public static class LC_287
+{
+    public static void Func()
+    {
+        //  Floyd判圈算法
+    }
+    
+    /*
+     *  给定一个包含 n + 1 个整数的数组 nums ，其数字都在 1 到 n 之间（包括 1 和 n），可知至少存在一个重复的整数。
+        假设 nums 只有 一个重复的整数 ，找出 这个重复的数
+     */
+    public static int FindDuplicate(int[] nums) 
+    {
+        /*
+        int sum = 0;
+        int length = nums.Length;
+        for(int i = 0; i < length; ++ i)
+        {
+            sum += nums[i];
+        }
+        sum = sum - length * (length - 1) / 2;
+        return sum;
+        */
+
+        // 将其看成是一个循环的链表，快慢指针循环
+        int index1 = 0;
+        int index2 = 0;
+        do
+        {
+            index1 = nums[index1];
+            index2 = nums[index2];
+            index2 = nums[index2];
+            
+        }while (nums[index1] != nums[index2]);
+
+        index1 = 0;
+        // 找出在哪个位置为起始点，可证必定在圆圈起点相遇
+        while(index1 != index2){
+            index1 = nums[index1];
+            index2 = nums[index2];
+        }
+        return index1;
+    }
+}
