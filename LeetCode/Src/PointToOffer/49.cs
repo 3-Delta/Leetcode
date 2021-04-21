b9655e73137e16870fc5151e3aec4eda94019048
kaclok <kaclok@users.noreply.github.com>
2021-04-22 00:46:56 +0800

    49. 字母异位词分组
    输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
    输出:
    [
      ["ate","eat","tea"],
      ["nat","tan"],
      ["bat"]
    ]
    
    来源：力扣（LeetCode）
    链接：https://leetcode-cn.com/problems/group-anagrams
    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。

--- LeetCode/Src/PointToOffer/49.cs
+++ LeetCode/Src/PointToOffer/49.cs
@@ -4,9 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public static class _49
-{
-    public static void Func()
-    {
+public static class _49 {
+    public static void Func() {
+        string[] strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+        GroupAnagrams(strs);
+    }
+
+    // string[] strs = new string[] { "eat", "tea", "tan", "ate", "nat", "bat" };
+    public static IList<IList<string>> GroupAnagrams(string[] strs) {
+        List<IList<string>> ret = new List<IList<string>>();
+
+        StringBuilder sb = new StringBuilder();
+        Dictionary<string, List<int>> dict = new Dictionary<string, List<int>>();
+        for (int i = 0, lengthI = strs.Length ; i < lengthI ; ++i) {
+            var str = strs[i];
+            int[] arr = new int[26];
+            // 主要是利用这个int[26]数组将无需的string转换为有序的char出现的次数的表达
+            // 然后从0到26-1便利为stringbuilder得到一个通用的string表达
+            for (int j = 0, lengthJ = str.Length ; j < lengthJ ; ++j) {
+                char c = str[j];
+                ++arr[c - 'a'];
+            }
+
+            for (int k = 0, length = 26 ; k < length ; ++k) {
+                sb.Append((char)(arr[k] + (int)'a'));
+            }
+
+            var s = sb.ToString();
+            sb.Clear();
+            if (!dict.TryGetValue(s, out List<int> ls)) {
+                ls = new List<int>();
+                dict.Add(s, ls);
+            }
+            ls.Add(i);
+        }
+
+        foreach (var kvp in dict) {
+            IList<string> ls = new List<string>();
+            ret.Add(ls);
+
+            for (int i = 0, length = kvp.Value.Count ; i < length ; ++i) {
+                ls.Add(strs[kvp.Value[i]]);
+            }
+        }
+
+        return ret;
     }
 }
