6ba4fe0b01e0b1342d2cfefa88e5a4371461dbf6
kaclok <kaclok@users.noreply.github.com>
2021-04-22 14:42:15 +0800

    leetcode:33, 34

---
+++ LeetCode/Src/LeetCode/Middle/33.cs
@@ -0,0 +1,37 @@
+﻿public static class LC_33 {
+    // 33. 搜索旋转排序数组
+    public static void Func() {
+        int[] ls = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+        int rlt = Search(ls, 1);
+    }
+
+    // 将数组一分为二，其中一定有一个是有序的，另一个可能是有序，也能是部分有序。
+    // 此时有序部分用二分法查找。无序部分再一分为二，其中一个一定有序，另一个可能有序，可能无序。就这样循环.
+    public static int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        // <=
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            // 左边整体有序
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid])
+                    right = mid - 1;
+                else
+                    left = mid + 1;
+            }
+            // 右边整体有序
+            else {
+                if (nums[mid] < target && target <= nums[right])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ LeetCode/Src/LeetCode/Middle/34.cs
@@ -0,0 +1,50 @@
+﻿public static class LC_34 {
+    public static void Func() {
+        int[] ls = new int[] { 5, 7, 7, 8, 8, 10 };
+        SearchRange(ls, 6);
+        SearchRange(ls, 8);
+    }
+
+    public static int[] SearchRange(int[] nums, int target) {
+        int begin = -1;
+        int end = -1;
+
+        // 先二分查找， 然后居中向左，向右查找
+        int index = BinarySearch(nums, 0, nums.Length - 1, target);
+        if (index != -1) {
+            begin = index;
+            for (int i = index - 1 ; i >= 0 ; i--) {
+                if (nums[i] == target) {
+                    begin = i;
+                }
+            }
+            end = index;
+            for (int i = end + 1 ; i < nums.Length ; i++) {
+                if (nums[i] == target) {
+                    end = i;
+                }
+            }
+        }
+
+        return new int[] { begin, end};
+    }
+
+    public static int BinarySearch(int[] nums, int l, int r, int target) {
+        if (nums != null && nums.Length > 0) {
+            while (l <= r) {
+                int mid = (l + r) / 2;
+                int v = nums[mid];
+                if (target == v) {
+                    return mid;
+                }
+                else if (target > v) {
+                    l = mid + 1;
+                }
+                else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
