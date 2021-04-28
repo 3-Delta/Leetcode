206045c2aeba1064dfc8eb6323ca0dbfe6f2da81
kaclok <kaclok@users.noreply.github.com>
2021-04-28 15:45:22 +0800

    633

---
+++ LeetCode/Src/LeetCode/Middle/633.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using System.Threading.Tasks;
+
+public static class LC_633
+{
+    public static void Func()
+    {
+        
+    }
+
+    public static bool JudgeSquareSum(int c) {
+        /*
+        for (long a = 0; a * a <= c; ++a) {
+            double b = Math.Sqrt(c - a * a);
+            // 精华在这里：b == (int) b
+            if (b == (int) b) {
+                return true;
+            }
+        }
+        return false;
+        */
+
+        long left = 0;
+        long right = (long)Math.Sqrt(c);
+        while (left <= right) {
+            long sum = left * left + right * right;
+            if (sum == c) {
+                return true;
+            }
+            else if (sum > c) {
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+        return false;
+    }
+}
